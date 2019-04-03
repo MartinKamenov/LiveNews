0c9706bac7aab875b0f17de76753e25a9d4b996e
MartinKamenov <martinkamenov10@gmail.com>
2019-04-03 21:22:55 +0300

    Added stylesheet configuration for news containers.

--- src/ui/news/NewsDetailsComponent.js
+++ src/ui/news/NewsDetailsComponent.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text, Image, View } from 'react-native';
+import { Text, Image, View, StyleSheet } from 'react-native';
 
 const NewsDetailsComponent = ({ news, handlePress }) => (
-    <View>
+    <View style={styles.container}>
         <Image 
-            style={{width: 66, height: 58}}
+            style={{width: '50%', height: 50, justifyContent: 'center'}}
             source={{uri: news.urlToImage}}/>
-        <Text onPress={() => handlePress(news.url)}>{news.title}</Text>
+        <Text
+            style={styles.title}
+            onPress={() => handlePress(news.url)}>{news.title}</Text>
     </View>
 );
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+        //width: '75%'
+    }
+});
 
 NewsDetailsComponent.propTypes = {
     news: PropTypes.shape({
