808f24b0a7078aaa5b8c91e34f2a98c86e7f5ea1
MartinKamenov <martinkamenov10@gmail.com>
2019-04-03 19:12:20 +0300

    Fixed paths for components.

--- App.js
+++ App.js
@@ -1,49 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View, ScrollView, ProgressBarAndroid } from 'react-native';
-// import apiService from './src/services/api-service';
+import React, { Component } from 'react';
+import NewsApp from './src/ui/NewsApp';
 
-export default class App extends React.Component {
-  state = {
-    isLoading: true,
-    news: []
-  };
-  componentDidMount() {
-    return fetch('https://fullcontrol-api.herokuapp.com/news/sport')
-      .then((response) => response.json())
-      .then((news) => {
-        this.setState({news, isLoading: false});
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
-  }
+export default class App extends Component {
   render() {
-    if(this.state.isLoading) {
-      return (
-        <View style={styles.container}>
-          <ProgressBarAndroid/>
-          <Text>Fetching news</Text>
-        </View>
-      );
-    }
     return (
-      <View style={styles.container}>
-        <Text>News</Text>
-        <ScrollView>
-          {this.state.news.map((singleNews, i) => (
-            <Text key={i}>{singleNews.title}</Text>
-          ))}
-        </ScrollView>
-      </View>
+      <NewsApp/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
