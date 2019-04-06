a9c527c79e84659fa4a865272ad3efc051ef5b63
MartinKamenov <martinkamenov10@gmail.com>
2019-04-06 21:10:36 +0300

    Getting images sources from type using require dynamicly.

--- src/ui/NewsApp.js
+++ src/ui/NewsApp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NewsComponent from './news/NewsComponent';
 import { Text, Image, View, StyleSheet, TouchableHighlight  } from 'react-native';
+import types from '../enums/types';
 
 class NewsAppComponent extends Component {
     state = {
@@ -10,20 +11,31 @@ class NewsAppComponent extends Component {
     handleSelectionOfType = (selectedType) => {
         this.setState({selectedType});
     }
+
+    getImageSourceFromType = (type) => {
+        console.log(type);
+        switch(type) {
+            case 'any':
+                return require('../../assets/news_carousel.jpg');
+            case 'sport':
+                return require('../../assets/sport_carousel.jpg');
+        }
+    }
     render() {
         if(!this.state.selectedType) {
             return (
                 <View>
-                    <TouchableHighlight onPress={() => this.handleSelectionOfType('any')}>
-                        <Image
-                            style={styles.carouselImage}
-                            source={require('../../assets/news_carousel.jpg')}/>
-                    </TouchableHighlight>
-                    <TouchableHighlight onPress={() => this.handleSelectionOfType('sport')}>
-                        <Image
-                            style={styles.carouselImage}
-                            source={require('../../assets/sport_carousel.jpg')}/>
-                    </TouchableHighlight>
+                    {types.map((type, i) => { 
+                        return (
+                            <TouchableHighlight
+                                key={i}
+                                onPress={() => this.handleSelectionOfType(type)}>
+                                <Image
+                                    style={styles.carouselImage}
+                                    source={this.getImageSourceFromType(type)}/>
+                            </TouchableHighlight>
+                        )}
+                    )}
                 </View>
             );
         }
@@ -38,8 +50,8 @@ class NewsAppComponent extends Component {
 
 const styles = StyleSheet.create({
     carouselImage: {
-        width: '50%',
-        height: 100
+        width: '100%',
+        height: '50%'
     }
 });
  
