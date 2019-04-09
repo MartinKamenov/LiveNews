87f81d1f4d382c3b85d245d650e6f6bd073823de
MartinKamenov <martinkamenov10@gmail.com>
2019-04-09 21:24:42 +0300

    Extracted NewsTypesRow component.

--- src/ui/main/NewsApp.js
+++ src/ui/main/NewsApp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchFormComponent from '../search/SearchFormComponent';
+import NewsTypesRowComponent from './NewsTypesRowComponent';
 import { Text, ImageBackground , View, StyleSheet, TouchableHighlight  } from 'react-native';
 import PropTypes from 'prop-types';
 import types from '../../enums/types';
@@ -7,7 +8,7 @@ import constants from '../../constants/constants';
 
 class NewsAppComponent extends Component {
     static navigationOptions = {
-        title: 'NewsApp',
+        title: 'LiveNews',
         headerTitleStyle: {
             color: '#ffffff'
         },
@@ -42,11 +43,11 @@ class NewsAppComponent extends Component {
         }
     }
     render() {
-        let copyOfTypes = Object.assign({}, types);
-        let rowOfTypes = [];
+        let copyOfTypes = types.slice();
+        let allRows = [];
         while (copyOfTypes.length > 0) {
-            chunk = news.splice(0, constants.typesOnRow);
-            rowOfTypes.push(chunk);
+            chunk = copyOfTypes.splice(0, constants.typesOnRow);
+            allRows.push(chunk);
         }
 
         return (
@@ -57,28 +58,16 @@ class NewsAppComponent extends Component {
                     handleSearch={this.handleSearch}
                     handleChangeSearchInput={this.handleChangeSearchInput}/>
 
-                <View
-                    style={styles.row}>
-                    {types.map((type, i) => { 
-                        return (
-                            <TouchableHighlight
-                                style={styles.inputWrap}
+                <View>
+                    {
+                        allRows.map((rowTypes, i) => (
+                            <NewsTypesRowComponent 
                                 key={i}
-                                onPress={() => this.handleSelectionOfType(type)}>
-                                <View>
-                                    <ImageBackground 
-                                        style={styles.carouselImage}
-                                        source={this.getImageSourceFromType(type)}
-                                        imageStyle={styles.carouselImageStyle}>
-                                        <View style={styles.textContainet}>
-                                            <Text style={styles.typeText}>{type.toUpperCase()}</Text>
-                                        </View>
-                                    </ImageBackground>
-                                </View>
-                                
-                            </TouchableHighlight>
-                        );}
-                    )}
+                                rowTypes={rowTypes}
+                                handleSelectionOfType={this.handleSelectionOfType}
+                                getImageSourceFromType={this.getImageSourceFromType}/>)
+                        )
+                    }
                 </View>
             </ImageBackground>
         );
@@ -89,41 +78,6 @@ const styles = StyleSheet.create({
     containerBackground: {
         width: '100%',
         height: '100%'
-    },
-    carouselImage: {
-        width: '100%',
-        height: 200
-    },
-    carouselImageStyle: {
-        borderColor: '#000000',
-        borderWidth: 2,
-        borderRadius: 50
-    },
-    typeText: {
-        color: '#0cff89',
-        fontWeight: 'bold',
-        backgroundColor: '#000000aa',
-        padding: 5
-    },
-    textContainet: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    row: {
-        flex: 1,
-        flexDirection: 'row',
-        height: 200
-    },
-    inputWrap: {
-        flex: 1,
-        marginBottom: 10,
-        width: '50%',
-        height: 200
     }
 });
 
